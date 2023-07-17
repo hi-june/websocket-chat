5ea3a424b917575bd1aa39beeaf5ae3027d60529
hi-june <wnsgur735@gmail.com>
2023-07-17 16:57:46 +0900

    feat: 채팅방 컨트롤러 정의

---
+++ chatting/src/main/java/com/june/chatting/domain/chat/controller/ChatRoomController.java
@@ -0,0 +1,46 @@
+package com.june.chatting.domain.chat.controller;
+
+import com.june.chatting.domain.chat.model.ChatRoom;
+import com.june.chatting.domain.chat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping("/room")
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomService.findAllChatRooms();
+    }
+
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomService.createChatRoom(name);
+    }
+
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomDetail";
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomService.findChatRoom(roomId);
+    }
+}
